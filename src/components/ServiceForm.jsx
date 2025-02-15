795dc78a2e47ea4775618057fa460c04bba8b8a2
nada7harby <87335429+nada7harby@users.noreply.github.com>
2025-02-15 20:22:48 +0200

    Update ServiceForm.jsx

--- src/components/ServiceForm.jsx
+++ src/components/ServiceForm.jsx
@@ -24,7 +24,11 @@ const ServiceForm = () => {
   };
 
   return (
-    <div style={{ backgroundColor: "rgb(0 121 96 / 19%)" }}>
+    <div
+      style={{ backgroundColor: "rgb(0 121 96 / 19%)" }}
+      data-aos="fade-up"
+      data-aos-duration={1000}
+    >
       <div className="container my-5">
         <div className="p-4">
           <h2 className="text-center mb-4">إضافة خدمات</h2>
